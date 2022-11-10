faf0ccdd3adbce8857a22665e3ae8dfb43dccdf5
Marcel <marhandrez@gmail.com>
2022-11-10 14:59:38 +0100

    resell platform generated token implemented

--- contracts/CloudaxNftMarketplace.sol
+++ contracts/CloudaxNftMarketplace.sol
@@ -13,9 +13,11 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 error InvalidAddress();
 error NoProceeds();
 error TransferFailed();
+error NotOwner();
 error QuantityRequired(uint256 quantity, uint256 minRequired);
 error QuantityDoesNotExist(uint256 availableQuantity);
 error InvalidItemId(uint256 itemId);
+error NotListed(uint256 tokenId);
 error ItemSoldOut(uint256 quantity, uint256 numSold);
 error InsufficientFund(uint256 price, uint256 allowedFund);
 error CannotSendFund(uint256 senderBalance, uint256 fundTosend);
@@ -32,6 +34,7 @@ contract CloudaxNftMarketplace is
     // Counters.Counter private _tokenId;
     Counters.Counter private _listedItemId;
     Counters.Counter private _totalItem;
+    Counters.Counter private _totalToken;
     Counters.Counter private _itemsSold;
     // Counters.Counter private _itemsSoldcontractBaseURI;
     // string public baseTokenURI;
@@ -47,11 +50,21 @@ contract CloudaxNftMarketplace is
     // structs for an item to be listed
     struct ListedItem {
         uint256 itemId;
+        uint256 tokenId;
         address payable fundingRecipient;
         uint256 price;
         uint32 quantity;
         uint32 numSold;
         uint32 royaltyBPS;
+    }
+
+    struct SoldItem {
+        uint256 tokenId;
+        uint256 itemId;
+        address payable buyer;
+        address payable creator;
+        string tokenBaseURI;
+        uint256 amountEarned;
     }
 
     // Structure to define auction properties
@@ -67,16 +80,24 @@ contract CloudaxNftMarketplace is
         uint256 bidCount; // Number of bid placed on the auction
     }
 
-    //Mapping of items to a Item struct
+    //Mapping of items to a ListedItem struct
     mapping(uint256 => ListedItem) public listedItems;
+
+    //Mapping of items to a SoldItem struct
+    mapping(uint256 => SoldItem) public soldItems;
+
     ///Mapping an Item to the token copies minted from it.
     mapping(uint256 => uint256) public tokenToListedItem;
+
     // The amount of funds that have been earned from selling the copies of a given item.
     mapping(uint256 => uint256) public depositedForItem;
+
     // Total amount earned by Seller... mapping of address -> Amount earned
     mapping(address => uint256) private indivivdualEarningInPlatform;
+
     //Mapping of Item to a TokenStatus struct
     mapping(uint256 => TokenStatus) private _idToItemStatus;
+
     // mapping(uint256 => uint256) public depositedForEdition;
 
     // ================================
@@ -90,6 +111,15 @@ contract CloudaxNftMarketplace is
         uint256 price,
         uint32 quantity,
         uint32 royaltyBPS,
+        uint256 time
+    );
+
+    ///Emitted when a Item is listed for sell
+    event TokenListed(
+        uint256 indexed tokenId,
+        uint256 indexed itemId,
+        address indexed owner,
+        uint256 price,
         uint256 time
     );
 
@@ -119,6 +149,8 @@ contract CloudaxNftMarketplace is
 
     event ServiceFeeUpgraded(uint256 oldFee, uint256 newFee, uint256 time);
 
+    event ItemListingCanceled(address owner, uint256 tokenId, uint256 time);
+
     enum TokenStatus {
         DEFAULT,
         ACTIVE,
@@ -129,6 +161,22 @@ contract CloudaxNftMarketplace is
 
     constructor() ERC721("Cloudax", "CLDX") {
         _contractBaseURI = "";
+    }
+
+    modifier isOwner(uint256 tokenId, address spender) {
+        address owner = ERC721.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(uint256 tokenId) {
+        ListedItem memory listing = listedItems[tokenId];
+        if (listing.price <= 0) {
+            revert NotListed(tokenId);
+        }
+        _;
     }
 
     // This function is used to update or set the CloudaxNFT Base for Opensea compatibiblity
@@ -152,6 +200,7 @@ contract CloudaxNftMarketplace is
     /// @param _quantity The maximum number of NFT item's copy or tokens that can be sold.
     /// @param _royaltyBPS The royalty amount in bps per copy of a NFT item.
     function _createItem(
+        uint256 _itemId,
         address payable _fundingRecipient,
         uint256 _price,
         uint32 _quantity,
@@ -163,16 +212,17 @@ contract CloudaxNftMarketplace is
         }
 
         // Check that the track's quantity is more than zero(0).
-        // require(_quantity > 0, "Track quantity must not be less than 0");
         if (_quantity <= 0) {
             revert QuantityRequired({quantity: _quantity, minRequired: 1});
         }
 
-        _listedItemId.increment();
+        // _listedItemId.increment();
+        // uint256 newItemId = _listedItemId.current();
         _totalItem.increment();
-        uint256 newItemId = _listedItemId.current();
+        uint256 newItemId = _itemId;
         listedItems[newItemId] = ListedItem({
             itemId: newItemId,
+            tokenId: 0,
             fundingRecipient: _fundingRecipient,
             price: _price,
             numSold: 0,
@@ -202,7 +252,9 @@ contract CloudaxNftMarketplace is
         uint256 _itemId,
         string memory _tokenBaseURI
     ) external payable nonReentrant {
-        _createItem(_fundingRecipient, _price, _quantity, _royaltyBPS);
+        _createItem(_itemId, _fundingRecipient, _price, _quantity, _royaltyBPS);
+        // get itId from the emitted event of the above "_createItem" methed
+
         // Caching variables locally to reduce reads
         uint256 price = listedItems[_itemId].price;
         uint32 quantity = listedItems[_itemId].quantity;
@@ -249,7 +301,9 @@ contract CloudaxNftMarketplace is
         // Update the deposited total for the item.
         // Adding the newly earned money to the total amount a user has earned from an Item.
         depositedForItem[_itemId] += finalAmount;
-        indivivdualEarningInPlatform[msg.sender] += finalAmount;
+        indivivdualEarningInPlatform[
+            listedItems[_itemId].fundingRecipient
+        ] += finalAmount;
         _platformEarning += serviceFee;
         _totalAmount += msg.value;
 
@@ -263,6 +317,15 @@ contract CloudaxNftMarketplace is
 
         _idToItemStatus[newTokenId] = TokenStatus.ACTIVE;
         _itemsSold.increment();
+
+        soldItems[newTokenId] = SoldItem({
+            tokenId: newTokenId,
+            itemId: _itemId,
+            buyer: payable(msg.sender),
+            creator: listedItems[_itemId].fundingRecipient,
+            tokenBaseURI: _tokenBaseURI,
+            amountEarned: finalAmount
+        });
 
         emit ItemCopySold(
             newTokenId,
@@ -276,100 +339,134 @@ contract CloudaxNftMarketplace is
         );
     }
 
-    function listedToken(uint256 tokenId, uint256 price) public {}
+    function listToken(
+        uint256 tokenId,
+        uint256 price,
+        address payable fundingRecipant
+    ) public isOwner(tokenId, msg.sender) {
+        uint256 itemId = tokenToListedItem[tokenId];
+        require(
+            fundingRecipant == msg.sender,
+            "Only the nft owner can receive"
+        );
+        approve(address(this), tokenId);
+        listedItems[tokenId] = ListedItem({
+            itemId: itemId,
+            tokenId: tokenId,
+            fundingRecipient: fundingRecipant,
+            price: price,
+            numSold: 0,
+            quantity: 0,
+            royaltyBPS: listedItems[itemId].royaltyBPS
+        });
+
+        _idToItemStatus[tokenId] = TokenStatus.ONSELL;
+
+        emit TokenListed(tokenId, itemId, msg.sender, price, block.timestamp);
+    }
+
+    function updateListing(uint256 tokenId, uint256 newPrice)
+        external
+        isListed(tokenId)
+        isOwner(tokenId, msg.sender)
+    {
+        require(
+            listedItems[tokenId].price > newPrice,
+            "New price should not be greater previous price"
+        );
+        listedItems[tokenId].price = newPrice;
+        emit TokenListed(
+            tokenId,
+            listedItems[tokenId].itemId,
+            msg.sender,
+            newPrice,
+            block.timestamp
+        );
+    }
+
+    function cancelListing(uint256 tokenId)
+        external
+        isOwner(tokenId, msg.sender)
+        isListed(tokenId)
+    {
+        delete (listedItems[tokenId]);
+        emit ItemListingCanceled(msg.sender, tokenId, block.timestamp);
+        _idToItemStatus[tokenId] = TokenStatus.ACTIVE;
+    }
 
     /// @notice Creates or mints a new copy or token for the given item, and assigns it to the buyer
-    /// @param _itemId The id of the item selected to be purchased
-    function buyNftItem(
-        address payable _fundingRecipient,
-        uint256 _price,
-        uint32 _royaltyBPS,
-        uint256 _itemId,
-        string memory _tokenBaseURI
-    ) external payable nonReentrant {
-        _createItem(_fundingRecipient, _price, _quantity, _royaltyBPS);
+    /// @param tokenId The id of the token selected to be purchased
+    function buyNft(uint256 tokenId, string memory _tokenBaseURI)
+        external
+        payable
+        nonReentrant
+        isListed(tokenId)
+    {
         // Caching variables locally to reduce reads
-        uint256 price = listedItems[_itemId].price;
-        uint32 quantity = listedItems[_itemId].quantity;
-        uint32 numSold = listedItems[_itemId].numSold;
+        uint256 price = listedItems[tokenId].price;
+        uint256 itemId = listedItems[tokenId].itemId;
 
         // Validations
-        // Check that the item's quantity is more than zero(0).
-        if (quantity <= 0) {
-            revert QuantityDoesNotExist({availableQuantity: quantity});
-        }
         // Check that the item's ID is not Zero(0).
-        if (_itemId <= 0) {
-            revert InvalidItemId({itemId: _itemId});
-        }
-        // Check that there are still some copies or tokens of the item that are available for purchase.
-        if (quantity >= numSold) {
-            revert ItemSoldOut({quantity: quantity, numSold: numSold});
+        if (itemId <= 0) {
+            revert InvalidItemId({itemId: itemId});
         }
         // Check that the buyer approved an amount that is equal or more than the price of the item set by the seller.
-        if (price > msg.value) {
+        if (price >= msg.value) {
             revert InsufficientFund({price: price, allowedFund: msg.value});
         }
 
-        // Increment the number of copies or tokens sold from this Item.
-        listedItems[_itemId].numSold++;
-        _listedItemId.increment();
-        _totalItem.increment();
-        uint256 newTokenId = _listedItemId.current();
-
-        // https://cloudaxnftmarketplace.xyz/metadata/[USER_ID]/[ITEM_ID]/[TOKEN_ID]
-        // Where _tokenBaseURI = https://cloudaxnftmarketplace.xyz/metadata/[USER_ID]/[ITEM_ID]
-        // Generating a metadata URL for the new copy of the ITEM or Token that was generated.
-
-        _tokenBaseURI = string(
-            abi.encodePacked(_tokenBaseURI, "/", Strings.toString(newTokenId))
-        );
-
-        //Deduct the service fee
+        //Deduct the fees
         uint256 serviceFee = msg.value.mul(_serviceFee).div(10000);
-        // uint256 royalty = msg.value.mul(_royaltyBPS).div(100);
-        // uint256 finalAmount = msg.value.sub(serviceFee + royalty);
-        uint256 finalAmount = msg.value.sub(serviceFee);
+        uint256 royalty = msg.value.mul(listedItems[tokenId].royaltyBPS).div(
+            100
+        );
+        uint256 finalAmount = msg.value.sub(serviceFee + royalty);
 
         // Update the deposited total for the item.
         // Adding the newly earned money to the total amount a user has earned from an Item.
-        depositedForItem[_itemId] += finalAmount;
-        indivivdualEarningInPlatform[msg.sender] += finalAmount;
+        depositedForItem[itemId] += finalAmount;
+        indivivdualEarningInPlatform[
+            listedItems[tokenId].fundingRecipient
+        ] += finalAmount;
         _platformEarning += serviceFee;
         _totalAmount += msg.value;
 
         // Send funds to the funding recipient.
-        _sendFunds(listedItems[_itemId].fundingRecipient, finalAmount);
+        _sendFunds(listedItems[tokenId].fundingRecipient, finalAmount);
 
-        // Mint a new copy or token from the Item for the buyer, using the `newTokenId`.
-        safeMint(msg.sender, newTokenId, _tokenBaseURI);
-        // Store the mapping of the ID a sold item's copy or token id to the Item being purchased.
-        tokenToListedItem[newTokenId] = _itemId;
+        // Send funds to the royalty to creator.
+        _sendFunds(soldItems[tokenId].creator, royalty);
 
-        _idToItemStatus[newTokenId] = TokenStatus.ACTIVE;
+        // transfer nft to buyer
+        safeTransferFrom(
+            listedItems[tokenId].fundingRecipient,
+            msg.sender,
+            tokenId
+        );
+
+        _idToItemStatus[tokenId] = TokenStatus.ACTIVE;
         _itemsSold.increment();
 
+        soldItems[tokenId] = SoldItem({
+            tokenId: tokenId,
+            itemId: itemId,
+            buyer: payable(msg.sender),
+            creator: soldItems[tokenId].creator,
+            tokenBaseURI: soldItems[tokenId].tokenBaseURI,
+            amountEarned: finalAmount
+        });
+
         emit ItemCopySold(
-            newTokenId,
-            _itemId,
-            listedItems[_itemId].numSold,
+            tokenId,
+            itemId,
+            0,
             msg.sender,
-            listedItems[_itemId].fundingRecipient,
-            _tokenBaseURI,
+            listedItems[tokenId].fundingRecipient,
+            soldItems[tokenId].tokenBaseURI,
             finalAmount,
             block.timestamp
         );
-
-        // emit nftItemSold(
-        //     newTokenId,
-        //     _itemId,
-        //     msg.sender,
-        //     listedItems[_itemId].fundingRecipient,
-        //     _tokenBaseURI,
-        //     msg.value,
-        //     price + msg.value,
-        //     block.timestamp
-        // );
     }
 
     /// @notice Sends funds to an address
